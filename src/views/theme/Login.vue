ff8a18eee9c994585f2722c23520e857d5993c50
Jeon <myeongsu@3c22fbccb5a0.ant.amazon.com>
2021-08-09 14:42:14 +0900

    Cognito Tst

--- src/views/theme/Login.vue
+++ src/views/theme/Login.vue
@@ -17,6 +17,7 @@ export default {
         bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
     }).then(user => console.log(user))
     .catch(err => console.log(err));
+    console.log("aaaa");
   }
 }
 </script>
