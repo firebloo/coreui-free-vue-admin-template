0da6a6e9f52835c8f45c9c6a8d0e07d41e2e419d
Jeon <myeongsu@3c22fbccb5a0.ant.amazon.com>
2021-08-10 22:42:43 +0900

    Cognito test

--- src/views/theme/auth.js
+++ src/views/theme/auth.js
@@ -5,12 +5,19 @@ import UserInfoStore from './user-info-store';
 import UserInfoApi from './user-info-api';
 
 
-const CLIENT_ID = process.env.VUE_APP_COGNITO_CLIENT_ID;
-const APP_DOMAIN = process.env.VUE_APP_COGNITO_APP_DOMAIN;
-const REDIRECT_URI = process.env.VUE_APP_COGNITO_REDIRECT_URI;
-const USERPOOL_ID = process.env.VUE_APP_COGNITO_USERPOOL_ID;
-const REDIRECT_URI_SIGNOUT = process.env.VUE_APP_COGNITO_REDIRECT_URI_SIGNOUT;
-const APP_URL = process.env.VUE_APP_APP_URL;
+// const CLIENT_ID = process.env.VUE_APP_COGNITO_CLIENT_ID;
+// const APP_DOMAIN = process.env.VUE_APP_COGNITO_APP_DOMAIN;
+// const REDIRECT_URI = process.env.VUE_APP_COGNITO_REDIRECT_URI;
+// const USERPOOL_ID = process.env.VUE_APP_COGNITO_USERPOOL_ID;
+// const REDIRECT_URI_SIGNOUT = process.env.VUE_APP_COGNITO_REDIRECT_URI_SIGNOUT;
+// const APP_URL = process.env.VUE_APP_APP_URL;
+
+const CLIENT_ID = "48q14q979ohgjg1kmt172uab2b";
+const APP_DOMAIN = "lamaabp.auth.us-east-1.amazoncognito.com";
+const REDIRECT_URI = "https://master.d18ewksbg2mt7e.amplifyapp.com/login/oauth2/code/cognito";
+const USERPOOL_ID = "lama-abp-cognito";
+const REDIRECT_URI_SIGNOUT = "https://master.d18ewksbg2mt7e.amplifyapp.com/logout";
+const APP_URL = "https://master.d18ewksbg2mt7e.amplifyapp.com";
 
 var authData = {
     ClientId : CLIENT_ID, // Your client id here
